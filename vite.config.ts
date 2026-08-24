bca2b2c4e426d40b2304212e471e5723910d2b07
Moïse Apata <146359171+Moiseapata@users.noreply.github.com>
2026-08-24 12:51:52 +0200

    Configure base path and output directory in Vite
    Set base path for assets and updated output directory.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',  // <-- C'est le réglage qui va faire fonctionner toutes tes images !
   build: {
-    outDir: './dist/public',
+    outDir: 'dist/public',
   }
 })
